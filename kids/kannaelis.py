2be1eba54243003c2e2e4d901726b76cdd8c7bbf
perlaas <pertlaas@gmail.com>
2017-12-25 14:55:48 +0100

    Add files via upload
    Added encoding

--- kids/kannaelis.py
+++ kids/kannaelis.py
@@ -1,15 +1,25 @@
+# coding=UTF-8
 svar = input("Vilken är Elis älsklingfärg?")
 if svar=="röd":
-  print("Rätt")
+   print("Rätt")
 else:
-  print(" %s är Fel" % svar)
+   print(" %s är Fel" % svar)
+
 svar = input("Vilken är Elis favoriträtt?")
 if svar=="kyckling":
-  print("Rätt")
+   print("Rätt")
 else:
-  print(" %s är Fel" % svar)
+   print(" %s är Fel" % svar)
+
 svar = input("hur gammal är elis ?")
 if svar=="9":
-  print("Rätt")
+   print("Rätt")
 else:
-  print(" %s är Fel" % svar)
+   print(" %s är Fel" % svar)
+
+svar = input("vad är elis favoritspel?")
+if svar=="minecraft":
+   print("Rätt")
+else:
+   print(" %s är Fel" % svar)
+
